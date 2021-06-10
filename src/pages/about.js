ac8af9851aeface6b29b59e103ee9af0d7304084
Erik Toelke <tolks@Eriks-MacBook-Pro.local>
2021-06-10 19:03:10 -0400

    gatsby part 2

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <div>
+    <Layout pageTitle="About Me">
+        <p>about me here</p>
+    </Layout>
+    </div>
+  )
+}
+// Step 3: Export your component
+export default AboutPage
+
+
